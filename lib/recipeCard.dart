05b14871ee8ae526e96fa214ca9dee81ca6f1bca
Akshat Shah <akshat1@mail.usf.edu>
2020-03-06 02:46:06 +0530

    Fixing the screen

--- lib/recipeCard.dart
+++ lib/recipeCard.dart
@@ -14,13 +14,9 @@ class _RecipesScreenState extends State<RecipesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Swiper(
-      itemBuilder: (BuildContext context, int index) {
-        return RecipeCard();
-      },
-      itemCount: 3,
-      scrollDirection: Axis.vertical,
-    ));
+      appBar: AppBar(title:Text("Recipes")),
+      body: RecipeCard(),
+    );
   }
 }
 
@@ -37,7 +33,7 @@ class _RecipeCardState extends State<RecipeCard> {
       recipeId: 69,
       recipeName: 'Sezdesdevet',
       recipeDuration: 69,
-     // recipeImageUrl: 'imagejpg',
+      // recipeImageUrl: 'imagejpg',
     );
     // createRecipes(recipes).then((response) {
     //   if (response.statusCode > 200)
@@ -51,48 +47,64 @@ class _RecipeCardState extends State<RecipeCard> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('Recipes')),
-        body: FutureBuilder<List<Recipes>>(
-            future: loadRecipes(),
-            builder: (context, snapshot) {
-              /*callAPI();*/
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasError) {
-                  return Text("Error");
-                }
-                return Swiper(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, i) => Center(
-                      child: Card(
-                          elevation: 5,
-                          margin: EdgeInsets.fromLTRB(20, 150, 20, 150),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0)),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              // Row(
-                              //   children: <Widget>[
-                              //     Expanded(
-                              //       child: Text(
-                              //           '${snapshot.data[i].recipeImageUrl}'),
-                              //     )
-                              //   ],
-                              // ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: <Widget>[
-                                  Text('${snapshot.data[i].recipeName}'),
-                                  Text('${snapshot.data[i].recipeDuration}'),
-                                ],
-                              )
-                            ],
-                          ))),
-                );
-              } else
-                return CircularProgressIndicator();
-            }));
+    return FutureBuilder<List<Recipes>>(
+        future: loadRecipes(),
+        builder: (context, snapshot) {
+          /*callAPI();*/
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Text("Error");
+            }
+            return Swiper(
+              scrollDirection: Axis.vertical,
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, i) => Center(
+                  child: RecipeCardItem(
+                name: snapshot.data[i].recipeName,
+                duration: snapshot.data[i].recipeDuration,
+              )),
+            );
+          } else
+            return CircularProgressIndicator();
+        });
+  }
+}
+
+class RecipeCardItem extends StatelessWidget {
+  final String name;
+  final int duration;
+  const RecipeCardItem({
+    this.name,
+    this.duration,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 5,
+        margin: EdgeInsets.fromLTRB(20, 150, 20, 150),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            // Row(
+            //   children: <Widget>[
+            //     Expanded(
+            //       child: Text(
+            //           '${snapshot.data[i].recipeImageUrl}'),
+            //     )
+            //   ],
+            // ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text(name),
+                Text(duration.toString()),
+              ],
+            )
+          ],
+        ));
   }
 }
